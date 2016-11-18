4617a1c9e38845defedddebe33837183fcaca62f
Erik Westlund <edbwestlund@gmail.com>
2016-11-17 19:56:23 -0600

    add query functionality

--- src/Cacheable.php
+++ src/Cacheable.php
@@ -11,7 +11,7 @@ trait Cacheable
      * @param  int $page a page number for pagination
      * @return string
      */
-    public function getCacheKey($page = null)
+    public function getCacheKey($page = null, $query = null)
     {
         $key = sprintf("%s/%s-%s",
             get_class($this),
@@ -19,6 +19,23 @@ trait Cacheable
             $this->updated_at->timestamp
         );
 
-        return $page ? $key . '-pg:' . $page : $key;
+        $suffixes = $this->getSuffixes($page, $query);
+
+        return $suffixes ? $key . '-' . $suffixes : $key;
+    }
+
+    protected function getSuffixes($page, $query, $suffixes)
+    {
+        $key_parts = [];
+
+        if($page) {
+            $key_parts[] = 'pg:' . $page;
+        }
+
+        if($query) {
+            $key_parts[] = 'query:' . md5($query);
+        }
+
+        return implode('-', $key_parts);
     }
 }
